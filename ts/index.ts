f22b24d0110ed1f46e9c001a41c1b711e8ed6207
hirumaki <index3583@gmail.com>
2021-05-06 18:02:01 +0900

    add gitignore

--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,10 @@
+import Vue from 'vue';
+
 const msg:string = 'HelloWorld';
 console.log(msg);
 console.log('HEllo!!');
+
+const Component = Vue.extend({
+    el:'#app',
+    data: {message:'Hello Vue!'}
+});
